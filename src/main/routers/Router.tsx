eaf7f131ac0caf51289710158ffa1b19625ccbf5
Ruby Yujin <feat.ruby@gmail.com>
2023-03-08 11:09:40 +0900

    feature : 로그인 페이지  view

--- src/main/routers/Router.tsx
+++ src/main/routers/Router.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
+import Login from "../pages/Login";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/login" element={""} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={""} />
     </Routes>
   );
